6d3bf6da380f779b1b47e6fde0e0e81ddffa3d97
puthsithamouern <puthsithamouern@gmail.com>
2025-08-04 12:05:39 +0700

    ✨ feat: init project

---
+++ swiftUi101/ShapUI.swift
@@ -0,0 +1,18 @@
+//
+//  ShapUI.swift
+//  swiftUi101
+//
+//  Created by Puthsitha Moeurn on 4/8/25.
+//
+
+import SwiftUI
+
+struct ShapUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapUI()
+}
---
+++ swiftUi101/TextUI.swift
@@ -0,0 +1,18 @@
+//
+//  TextUI.swift
+//  swiftUi101
+//
+//  Created by Puthsitha Moeurn on 4/8/25.
+//
+
+import SwiftUI
+
+struct TextUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextUI()
+}
