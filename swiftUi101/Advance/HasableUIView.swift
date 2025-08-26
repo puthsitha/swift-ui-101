bd4cfff40444c8588becfa146ece7de34a44a4c0
puthsithamouern <puthsithamouern@gmail.com>
2025-08-26 16:34:25 +0700

    feat: implement hasable

---
+++ swiftUi101/Advance/HasableUIView.swift
@@ -0,0 +1,18 @@
+//
+//  HasableUIView.swift
+//  swiftUi101
+//
+//  Created by Puthsitha Moeurn on 26/8/25.
+//
+
+import SwiftUI
+
+struct HasableUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HasableUIView()
+}
